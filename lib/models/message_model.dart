806bb0433c52d86abcf926e95e5860eb55f33273
FaizinAjah <faizinajah13@gmail.com>
2025-01-09 14:54:23 +0700

    progress main.dart

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-class SignInScreen extends StatelessWidget {
+class Message {
   
 }
---
+++ lib/screens/messages_screen.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class MessagesScreen extends StatelessWidget {
+  
+}
